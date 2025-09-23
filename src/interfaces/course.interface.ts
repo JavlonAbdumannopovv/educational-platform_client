5f77f1f1e517e643e2fd4be2bbfd50f33aa50200
JavlonAbdumannopovv <javlonabdumannopovv@gmail.com>
2025-09-23 20:07:38 +0500

    #139. Detailed Course

--- src/interfaces/course.interface.ts
+++ src/interfaces/course.interface.ts
@@ -1,25 +1,27 @@
 export interface CourseType {
-	slug: string;
-	previewImage: string;
-	title: string;
-	lessonCount: number;
-	totalHour: number;
-	level: string;
-	price: number;
-	reviewAvarage: number;
-	reviewCount: number;
-	author: AuthorType;
-	tags: string[];
-	requirements: string[];
-	learn: string[];
-	exerpt: string;
-	description: string;
-	category: string;
-	_id: string;
-	language: string;
-	isActive: boolean;
+  slug: string;
+  previewImage: string;
+  title: string;
+  lessonCount: number;
+  totalHour: number;
+  level: string;
+  price: number;
+  reviewAvarage: number;
+  reviewCount: number;
+  author: AuthorType;
+  tags: string[];
+  requirements: string[];
+  learn: string[];
+  exerpt: string;
+  description: string;
+  category: string;
+  _id: string;
+  language: string;
+  isActive: boolean;
+  updatedAt: Date;
 }
 export interface AuthorType {
-	fullName: string;
-	avatar: string;
+  fullName: string;
+  avatar: string;
+  job: string;
 }
--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -1,24 +1,32 @@
-import axios from 'axios';
-import { API_URL, getCourseUrl, getInstructorurl } from 'src/config/api.config';
+import axios from "axios";
+import { API_URL, getCourseUrl, getInstructorurl } from "src/config/api.config";
 
 export const AppService = {
-	async getMainPageSource(language?: string) {
-		const { data: courses } = await axios.get(
-			`${API_URL}${getCourseUrl('all')}?language=${language}&limit=6`
-		);
+  async getMainPageSource(language?: string) {
+    const { data: courses } = await axios.get(
+      `${API_URL}${getCourseUrl("all")}?language=${language}&limit=6`
+    );
 
-		const { data: instructors } = await axios.get(
-			`${API_URL}${getInstructorurl('all')}?language=${language}&limit=6`
-		);
+    const { data: instructors } = await axios.get(
+      `${API_URL}${getInstructorurl("all")}?language=${language}&limit=6`
+    );
 
-		return { courses, instructors };
-	},
+    return { courses, instructors };
+  },
 
- 	async getCourses(language?: string, limit: string = '10') {
-		const { data: courses } = await axios.get(
-			`${API_URL}${getCourseUrl('all')}?language=${language}&limit=${limit}`
-		);
+  async getCourses(language?: string, limit: string = "10") {
+    const { data: courses } = await axios.get(
+      `${API_URL}${getCourseUrl("all")}?language=${language}&limit=${limit}`
+    );
 
-		return courses;
-	},
+    return courses;
+  },
+
+  async getDetailedCourse(slug?: string) {
+    const { data } = await axios.get(
+      `${API_URL}${getCourseUrl("detailed-course")}/${slug}`
+    );
+
+    return data;
+  },
 };
